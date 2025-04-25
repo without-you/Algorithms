9a11f5832a2e8d007c0890f73677bd2abfe696bb
韩笑笑 <1426463897@qq.com>
2025-04-25 22:15:55 +0800

    new jar about algs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+
+import edu.princeton.cs.algs4.StdIn;
 public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
